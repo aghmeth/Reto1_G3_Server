8176bfc4272dfbf872877c7aae06eaa37138393e
2dam <2dam@HZ375754.TartangaLH.eus>
2022-11-07 10:11:22 +0100

    commit de pool de conexiones y parte del socket. Tambien config de reto1 modificada

--- src/damc/grupo3/reto1/model/Pool.java
+++ src/damc/grupo3/reto1/model/Pool.java
@@ -5,10 +5,91 @@
  */
 package damc.grupo3.reto1.model;
 
+import damc.grupo3.reto1.exception.NoOperativeDataBaseException;
+import damc.grupo3.reto1.exception.TimeOutException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import java.util.Stack;
+import java.util.logging.Logger;
+
 /**
  *
  * @author 2dam
  */
 public class Pool {
+     private static final Logger LOG = Logger.getLogger(Pool.class.getName());
+    // fichero config.properties
+    private ResourceBundle configFile;
+    private String driverBD;
+    private String urlDB;
+    private String userDB;
+    private String passDB;
+
+    private static Pool pool;
+    // en este tipo de colecion
+    private Stack<Connection> poolStack;
+
+    /**
+     * Metodo para hacer la conexion con la base de datos
+     *
+     * 
+     */
+    public void openConnection() throws NoOperativeDataBaseException {
+        this.configFile = ResourceBundle.getBundle("model.Reto1");
+        this.driverBD = configFile.getString("DRIVERDB");
+        this.urlDB = configFile.getString("URLDB");
+        this.userDB = configFile.getString("USERDB");
+        this.passDB = configFile.getString("PASSDB");
+        try {
+            Connection conn = DriverManager.getConnection(urlDB, userDB, passDB);
+            //push añadir una conexion a la pila 
+            poolStack.push(conn);
+        } catch (SQLException e) {
+            throw new NoOperativeDataBaseException(urlDB);
+        }
+    }
+    /**
+     * creamos la colecion para almacenar las conexiones
+     * Lanzamos excepciones
+     *
+     */
+    public void createStackPool() throws SQLException, NoOperativeDataBaseException {
+        //Creamos  un Stack donde se alcenaran las conexiones.
+        poolStack = new Stack<>();
+        this.openConnection();
+    }
+/**
+ * 
+ * @return controla que solo haya un pool conection creado
+ */
+    public static Pool poolInstance(){
+        if (pool == null) {
+            pool = new Pool();
+            return pool;
+        } else {
+           return pool;
+        }
+    }
+    /**
+     * Abrimos una conexion del stack que hemos creado anteriormente
+     * 
+     */
+    public Connection getConnection() throws TimeOutException {
+        Connection conn = null;
+        if (poolStack.size() > 0) {
+            conn = poolStack.pop();
+        }
+        return conn;
+    }
+    /**
+     *cerrar una conexion cuando sea necesario
+     *
+     */
+    public void closeConnection(Connection con) throws TimeOutException {
+        LOG.info("Cerrar una conexion ");
+        poolStack.push(con);
+    }
     
 }
--- src/damc/grupo3/reto1/model/SignerServer.java
+++ src/damc/grupo3/reto1/model/SignerServer.java
@@ -5,10 +5,33 @@
  */
 package damc.grupo3.reto1.model;
 
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import java.util.logging.Logger;
+
 /**
  *
  * @author 2dam
  */
-public class SignerServer {
-    
+public class SignerServer extends Thread{
+    private static final ResourceBundle RETO1 = ResourceBundle.getBundle("model.reto1");
+    private static final int MAX_USERS = Integer.parseInt(RETO1.getString("MaxUsers"));
+    private static final int PORT = Integer.parseInt(RETO1.getString("PORT"));
+
+    private static ArrayList<SignerThread> receiveClients = new ArrayList<>();
+
+    private static final Logger LOGGER = Logger.getLogger(SignerServer.class.getName());
+
+    private static boolean serverOn = true;
+    private static ServerSocket svSocket;
+
+    /**
+     * Method use to start the server
+     *
+     */
+
+
 }
